c7eb1b1aef6d3ce3679b3a9ca71fa81afea08bd1
rota99 <rota99@users.noreply.github.com>
2021-06-28 23:05:35 +0200

    non so se riesco

--- Assets/Script/witch_movement.cs
+++ Assets/Script/witch_movement.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class witch_movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+  //variabili pubbliche
+  public float speed;                       //velocità
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    float modificatore;
+
+    if(Input.GetAxis("Horizontal") != 0 && Input.GetAxis("Vertical") != 0) {
+      //se sia l'asse orizzontale che verticale sono diversi da 0, i due assi premuti sono diversi
+      modificatore = Mathf.Sqrt(2);
+    }
+    else {
+      modificatore = 1;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //se l'utente vuole andare a destra e il personaggio è rivolto verso sinistra,
+    //oppure se l'utente vuole andare a sinistra e il personaggio è rivolto a destra,
+    //allora lo giro nella direzione corretta
+    if(((Input.GetKey("right") || Input.GetKey("d")) && transform.localScale.x < 0) || ((Input.GetKey("left") || Input.GetKey("a")) && transform.localScale.x > 0))
+      transform.localScale = new Vector3(transform.localScale.x*(-1), transform.localScale.y, transform.localScale.z);
+
+    transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"))*Time.deltaTime*speed/modificatore;
+  }
 }
