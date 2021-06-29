f89268620ee84c5b75f8b662f27ddd5be00e832c
rota99 <rota99@users.noreply.github.com>
2021-06-29 23:55:16 +0200

    prova cambio scena da menù iniziale

---
+++ Assets/Script/scene_changer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger: MonoBehaviour {
+
+  public void WordGaia() {
+    SceneManager.LoadScene("Word Gaia");
+  }
+
+  /*public void Scene2() {
+    SceneManager.LoadScene("Scene2");
+  }
+
+  public void Scene3() {
+    SceneManager.LoadScene("Scene3");
+  }*/
+}
