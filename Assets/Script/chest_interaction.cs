8185b74979255b109a842db4ee9d6558e02d9b0a
rota99 <rota99@users.noreply.github.com>
2021-07-01 17:06:52 +0200

    piccolo update

--- Assets/Script/chest_interaction.cs
+++ Assets/Script/chest_interaction.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class chest_interaction : MonoBehaviour
 {
-  //public AudioClip chestOpen;
-  //public AudioClip chestClose;
-
-  // Start is called before the first frame update
-  void Start()
-  {
-    //chestOpen = gameObject.AddComponent<AudioSource>();
-    //chestClose = gameObject.AddComponent<AudioSource>();
-  }
+  public GameObject open_sound;
+  public GameObject close_sound;
+  public GameObject potion_found_message;
 
   // Update is called once per frame
   void Update()
@@ -21,13 +15,18 @@ public class chest_interaction : MonoBehaviour
   }
 
   void OnTriggerStay2D(Collider2D chest) {
-    //chest.GetComponent<Animator>().SetBool("chest_opened", true);
-
     if(Input.GetKey("x")) {
       chest.GetComponent<Animator>().SetBool("chest_opened", true);
+      chest.transform.Find("chest_open_sound").GetComponent<AudioSource>().Play();
+      potion_found_message.rectTransform.localScale = Vector3(0.05, 0.09, 0);
     }
-    else {
+  }
+
+  void OnTriggerExit2D(Collider2D chest) {
+    if(chest.GetComponent<Animator>().GetBool("chest_opened")) {
       chest.GetComponent<Animator>().SetBool("chest_opened", false);
+      chest.transform.Find("chest_close_sound").GetComponent<AudioSource>().Play();
+      potion_found_message.rectTransform.localScale = Vector3(0, 0, 0);
     }
   }
 }
