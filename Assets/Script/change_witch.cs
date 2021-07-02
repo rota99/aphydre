0087a5f8d332e610c45c4d6861dfa51c189121ef
rota99 <rota99@users.noreply.github.com>
2021-07-02 23:08:45 +0200

    cambio status bar

--- Assets/Script/change_witch.cs
+++ Assets/Script/change_witch.cs
@@ -5,51 +5,43 @@ using UnityEngine;
 public class change_witch : MonoBehaviour
 {
   public GameObject[] witchArray = new GameObject[3];
+  public GameObject b_witch_img;
+  public GameObject w_witch_img;
+  public GameObject r_witch_img;
 
   float[] positionNegArray = new float[3] {0f, -200f, -400f};
   float[] positionPosArray = new float[3] {0f, 200f, 400f};
 
-//  GameObject[] witchArray2 = new GameObject[2];
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    /*  if(witchArray[2] == null) {
-        witchArray2[0] = witchArray[0];
-        witchArray2[1] = witchArray[1];
-      }*/
-    }
+  // Update is called once per frame
+  void Update()
+  {
+    //premi spazio per cambiare strega
+    if(Input.GetKeyDown("space")) {
+      GameObject t = witchArray[witchArray.Length - 1];
+      for(int i = witchArray.Length - 1; i > 0; i--)
+        witchArray[i] = witchArray[i - 1];
+      witchArray[0] = t;
 
-    // Update is called once per frame
-    void Update()
-    {
-      //premi spazio per cambiare strega
-      if(Input.GetKeyDown("space")) {
-        //if(witchArray2 == null || witchArray2.Length == 0) {
-          GameObject t = witchArray[witchArray.Length - 1];
-          for(int i = witchArray.Length - 1; i > 0; i--)
-            witchArray[i] = witchArray[i - 1];
-          witchArray[0] = t;
+      if(witchArray[0].transform.name.Contains("B_")) {
+        b_witch_img.SetActive(true);
+        w_witch_img.SetActive(false);
+        r_witch_img.SetActive(false);
+      }
+      else if(witchArray[0].transform.name.Contains("W_")) {
+        b_witch_img.SetActive(false);
+        w_witch_img.SetActive(true);
+        r_witch_img.SetActive(false);
+      }
+      else if(witchArray[0].transform.name.Contains("R_")) {
+        b_witch_img.SetActive(false);
+        w_witch_img.SetActive(false);
+        r_witch_img.SetActive(true);
+      }
 
-          for(int i = 0; i <= witchArray.Length - 1; i++) {
-            witchArray[i].transform.localPosition = new Vector2(positionNegArray[i], witchArray[i].transform.localPosition.y);
-          }
-        /*}
-        else{
-          GameObject t = witchArray2[witchArray2.Length - 1];
-          for(int i = witchArray2.Length - 1; i > 0; i--)
-            witchArray2[i] = witchArray2[i - 1];
-          witchArray2[0] = t;
-
-          for(int i = 0; i < witchArray2.Length - 1; i++) {
-            if(transform.localScale.x < 0) {
-              witchArray2[i].transform.position = new Vector3(positionNegArray[i], witchArray2[i].transform.position.y, witchArray2[i].transform.position.z);
-            }
-            else {
-              witchArray2[i].transform.position = new Vector3(positionPosArray[i], witchArray2[i].transform.position.y, witchArray2[i].transform.position.z);
-            }
-          }
-        }*/
+      for(int i = 0; i <= witchArray.Length - 1; i++) {
+        witchArray[i].transform.localPosition = new Vector2(positionNegArray[i], witchArray[i].transform.localPosition.y);
       }
     }
+  }
 }
