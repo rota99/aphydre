84e22929ef6781beceb0ecf0a6801fd3229c9940
Gaia96 <gaia.lorenzoni.96@gmail.com>
2021-07-03 18:30:28 +0200

    altri fix

--- Assets/Script/npc_interaction1.cs
+++ Assets/Script/npc_interaction1.cs
@@ -6,6 +6,7 @@ public class npc_interaction1 : MonoBehaviour
 {
     public string[] frasi_suore = new string[4];
     public string[] frasi_plebei = new string[7];
+    public string[] frase_soldato = new string[1];
     public GameObject npc_message;
 
     // Start is called before the first frame update
@@ -44,6 +45,19 @@ public class npc_interaction1 : MonoBehaviour
                 npc_message.SetActive(true);
 
                 npc_message.transform.GetChild(0).GetComponent<UnityEngine.UI.Text>().text = frasi_plebei[i].ToString();
+            }
+        }
+
+        if (npc.transform.name.Contains("soldato"))
+        {
+            if (Input.GetKeyDown("x"))
+            {
+                //genero un numero casuale compreso tra 0 e 3
+                int i = Random.Range(0, frase_soldato.Length);
+
+                npc_message.SetActive(true);
+
+                npc_message.transform.GetChild(0).GetComponent<UnityEngine.UI.Text>().text = frase_soldato[i].ToString();
             }
         }
     }
