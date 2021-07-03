2333f69c9a0a19dce44ede328b98c06962a31921
rota99 <rota99@users.noreply.github.com>
2021-07-03 19:40:48 +0200

    boss battle

--- Assets/Script/camera_follow.cs
+++ Assets/Script/camera_follow.cs
@@ -10,6 +10,6 @@ public class camera_follow : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
-    transform.position = player.position + offset; 
+    transform.position = player.position + offset;
   }
 }
