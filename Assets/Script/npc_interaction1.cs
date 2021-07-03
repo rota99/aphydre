b32a8fb1cae05d27be44be93abc770c945f79832
Gaia96 <gaia.lorenzoni.96@gmail.com>
2021-07-03 18:15:36 +0200

    fix dialoghi npc script

---
+++ Assets/Script/npc_interaction1.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npc_interaction1 : MonoBehaviour
+{
+    public string[] frasi_suore = new string[4];
+    public string[] frasi_plebei = new string[7];
+    public GameObject npc_message;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerStay2D(Collider2D npc)
+    {
+        if (npc.transform.name.Contains("suora"))
+        {
+            if (Input.GetKeyDown("x"))
+            {
+                //genero un numero casuale compreso tra 0 e 3
+                int i = Random.Range(0, frasi_suore.Length);
+
+                npc_message.SetActive(true);
+
+                npc_message.transform.GetChild(0).GetComponent<UnityEngine.UI.Text>().text = frasi_suore[i].ToString();
+            }
+        }
+
+        if (npc.transform.name.Contains("plebeo"))
+        {
+            if (Input.GetKeyDown("x"))
+            {
+                //genero un numero casuale compreso tra 0 e 3
+                int i = Random.Range(0, frasi_plebei.Length);
+
+                npc_message.SetActive(true);
+
+                npc_message.transform.GetChild(0).GetComponent<UnityEngine.UI.Text>().text = frasi_plebei[i].ToString();
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D npc)
+    {
+        npc_message.SetActive(false);
+    }
+}
