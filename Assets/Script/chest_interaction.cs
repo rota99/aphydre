5e92c714a3ba41019c62643c510b8bbfc95bff1e
rota99 <rota99@users.noreply.github.com>
2021-07-05 03:25:59 +0200

    boh vabbè

--- Assets/Script/chest_interaction.cs
+++ Assets/Script/chest_interaction.cs
@@ -8,10 +8,6 @@ public class chest_interaction : MonoBehaviour
   public GameObject open_sound;
   public GameObject close_sound;
   public GameObject potion_found_message;
-  public GameObject hp_parent;
-  public GameObject hp_prefab;
-
-  string[] chests = new string[8] {"","","","","","","",""};
 
   // Update is called once per frame
   void Update()
@@ -20,23 +16,12 @@ public class chest_interaction : MonoBehaviour
   }
 
   void OnTriggerStay2D(Collider2D chest) {
-    if (chest.transform.name.Contains("chest") && Array.IndexOf(chests, chest.transform.name) < 0) {
+    if (chest.transform.name.Contains("chest")) {
       if (Input.GetKey("x")) {
-        chests[Array.IndexOf(chests, "")] = chest.transform.name;
 
         chest.GetComponent<Animator>().SetBool("chest_opened", true);
         chest.transform.Find("chest_open_sound").GetComponent<AudioSource>().Play();
         potion_found_message.SetActive(true);
-
-        //GameObject hp = Instantiate(hp_prefab) as GameObject;
-        //hp.transform.parent = hp_parent.transform;
-
-        GameObject hp = Instantiate(hp_prefab);
-        float posx = -37 - (-2 * (hp_parent.transform.childCount - 1));
-
-        hp.transform.position = new Vector2((float)posx, 19.55f);
-
-        hp.transform.SetParent(hp_parent.transform, false);
       }
     }
   }
