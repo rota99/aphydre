5fd6361e91a833e5ec694d9036fc7be01e433870
rota99 <rota99@users.noreply.github.com>
2021-07-05 02:32:21 +0200

    boss

--- Assets/Script/enemy_hit.cs
+++ Assets/Script/enemy_hit.cs
@@ -9,6 +9,7 @@ public class enemy_hit : MonoBehaviour
 
   float waterTime;
   GameObject enemy;
+  int i = 0;
 
 
     // Start is called before the first frame update
@@ -32,7 +33,11 @@ public class enemy_hit : MonoBehaviour
     }
 
     void OnTriggerEnter2D(Collider2D enemy) {
+      i++;
       enemy.GetComponent<Animator>().SetTrigger("TakeDamage");
+
+      GameObject.Find("enemy_life").GetComponent<SpriteRenderer>().size -= new Vector2(0.5f, 0f);
+      GameObject.Find("enemy_life").transform.position += new Vector3(-1.5f * -i, 0f, 0f);
 
       if(!this.gameObject.transform.name.Contains("water") && enemy.transform.name.Contains("enemy")) {
         GameObject attackEnd = Instantiate(attackEnd_prefab);
